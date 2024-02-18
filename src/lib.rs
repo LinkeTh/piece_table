128d9245ae5ac25baf30b8fd02c82ec1bfc5df48
Thomas Linke <t.linke@pripares.com>
2024-02-18 13:54:12 +0100

    chore: cleanup fix: add pub text fn

--- src/lib.rs
+++ src/lib.rs
@@ -174,11 +174,9 @@ impl PieceTable {
         length
     }
 
-    fn display(&self) -> String {
+    pub fn text(&self) -> String {
         let mut result = String::new();
         for piece in &self.pieces {
-            // println!("display {:?}", piece);
-
             let content = match piece.buffer {
                 Buffer::Original => &self.original[piece.offset..piece.offset + piece.length],
                 Buffer::Add => &self.add[piece.offset..piece.offset + piece.length],
@@ -191,7 +189,7 @@ impl PieceTable {
 
 impl Display for PieceTable {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{}", self.display())
+        write!(f, "{}", self.text())
     }
 }
 
@@ -202,49 +200,13 @@ mod tests {
     #[test]
     fn test_it() {
         let mut piece_table = PieceTable::new("ipsum sit amet".to_string());
-        // let mut piece_table = PieceTable::new("For instance, to get the value of Index(15), the 3rd entry of piece table is retrieved. This is because the 3rd \
-        // entry describes the characters from index 11 to 16 (the first entry describes characters in index 0 to 5, the next one is 6 to 10). The piece table entry instructs \
-        // the program to look for the characters in the \"add file\" buffer, starting at index 17 in that buffer. The relative index in that entry is 15-11 = 4, which is added \
-        // to the start position of the entry in the buffer to obtain index of the letter: 4+17 = 21. The value of Index(15) is the 21st character of the \"add file\" buffer, \
-        // which is the character \"o\".".to_string());
 
         piece_table.insert(0, "Lorem ");
         piece_table.insert(6, "deletedtext");
         piece_table.delete(6, "deletedtext".len());
         piece_table.insert(11, " dolor");
 
-        assert_eq!("Lorem ipsum dolor sit amet", piece_table.display());
+        assert_eq!("Lorem ipsum dolor sit amet", piece_table.text());
         assert_eq!('o', piece_table.char_at(15).unwrap());
-
-        /*println!("1. test -> {}", piece_table);
-        println!("{:?}", piece_table.piece_at(15));
-        println!("{:?}", piece_table.char_at(15));
-
-        piece_table.insert(30, " at end");
-
-        println!("2.test -> {}", piece_table);
-
-        piece_table.insert(25, "0xxxxx0");
-        piece_table.insert(20, "1xxxxx1");
-        piece_table.insert(50, "2xxxxx2");
-        piece_table.insert(10, "3xxxxx3");
-        println!("3.test -> {}", piece_table);
-
-        piece_table.insert(11, "a");
-        piece_table.insert(12, "b");
-        piece_table.insert(13, "c");
-        piece_table.insert(14, "d");
-        piece_table.insert(15, "a");
-        piece_table.insert(15, "b");
-        piece_table.insert(15, "c");
-        piece_table.insert(15, "d");
-        println!("4.test -> {}", piece_table);
-
-        piece_table.delete(5, 38);
-
-        println!("5.test -> {}", piece_table);
-
-        piece_table.delete(0, 1);
-        println!("6.test -> {}", piece_table);*/
     }
 }
